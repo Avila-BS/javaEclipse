28bde01cf315367f2d40b6207c7de1401f210e89
Avila-BS <avila.xworkz@gmail.com>
2021-11-22 09:47:57 +0530

    interface examples

---
+++ Interface-Project/src/com/xworkz/interfacee/hospitalRules/HospitalRules.java
@@ -0,0 +1,10 @@
+package com.xworkz.interfacee.hospitalRules;
+
+public interface HospitalRules {
+	
+	public String register();
+	
+	public double payAdvance();
+	
+	public boolean validInsurance();
+}
